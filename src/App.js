835647da10d15dda43cf1f5596773baa1dd585a3
nastaran7911 <nastaran7911@gmail.com>
2025-04-12 17:50:53 +0330

    Added the Github ripo  URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather app</h1>
+      <footer>
+        This project is coded by Nastaran and is
+        <a
+          href="https://github.com/nastaran7911/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
