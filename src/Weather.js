9f0df1fa30c5f1c3308dc9f882e2a4cab15dc614
nastaran7911 <nastaran7911@gmail.com>
2025-04-12 19:09:18 +0330

    Added more html

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter City"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>New York</h1>
       <ul>
         <li>Wednesday 7:00</li>
