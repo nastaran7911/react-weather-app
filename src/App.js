fca453e7058cf80a298472040be88078626c247a
nastaran7911 <nastaran7911@gmail.com>
2025-04-12 18:26:24 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        This project is coded by Nastaran and is
-        <a
-          href="https://github.com/nastaran7911/react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather app</h1>
+        <footer>
+          This project is coded by Nastaran and is
+          <a
+            href="https://github.com/nastaran7911/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
