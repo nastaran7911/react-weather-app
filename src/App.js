e269cbecb3d1c68f239313239d30d6864c542917
nastaran7911 <nastaran7911@gmail.com>
2025-04-12 18:55:53 +0330

    Added the image

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        
         <Weather />
         <footer>
           This project is coded by Nastaran and is
           <a
             href="https://github.com/nastaran7911/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             open-sourced on Github
